a824c66695a6312b72758acd0797a5f136fb72be
s1monkol <p1199kl@gmail.com>
2026-06-19 16:07:34 +0200

    /recipes new page

---
+++ app/recipes/page.tsx
@@ -0,0 +1,19 @@
+import Spaghetti from "@/components/products/spaghetti";
+import Conchiglie from "@/components/products/conchiglie";
+import Vermicelli from "@/components/products/vermicelli";
+import Penne from "@/components/products/penne";
+import Fusilli from "@/components/products/fusilli";
+import Footer from "@/components/ui/footer";
+
+export default function RecipesPage() {
+  return (
+    <>
+      <Spaghetti />
+      <Conchiglie />
+      <Vermicelli />
+      <Penne />
+      <Fusilli />
+      <Footer />
+    </>
+  );
+}
