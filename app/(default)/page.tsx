ff22edf48e333259c2bf0d09830ec73845acd54d
s1monkol <p1199kl@gmail.com>
2026-06-19 16:24:19 +0200

    Default_page_recipes_transition

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -5,22 +5,13 @@ export const metadata = {
 
 
 import Hero from "@/components/hero-home";
-import Penne from "@/components/products/penne";
-import Fusilli from "@/components/products/fusilli";
-import Spaghetti from "@/components/products/spaghetti";
-import Conchiglie from "@/components/products/conchiglie";
-import Vermicelli from "@/components/products/vermicelli";
+
 
 export default function Home() {
   return (
     <>
       
       <Hero />
-      <Spaghetti />
-      <Conchiglie />
-      <Vermicelli />
-      <Penne />
-      <Fusilli />
 
     </>
   );
